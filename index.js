f448db460ddb937856c65efb17d26a92fe5e17ba
omarfarukee <umarfaruque1920@gmail.com>
2022-09-03 18:16:48 +0600

    Blog done

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 fetch('https://openapi.programming-hero.com/api/news/categories')
 .then(res => res.json())
 .then(data =>  newsCategory(data.data.news_category))
-    
-    const newsCategory = category =>{
-    console.log(category)
 
+const loadedCategory = async () => {
+    const url = 'https://openapi.programming-hero.com/api/news/categories';
+try{
+    const res = await fetch(url);
+    const data = await res.json();
+    newsCategory(data.data.news_category);  
+}
+catch(error){
+    console.log(error);
+}
+}
+ loadedCategory()
+ const newsCategory = category =>{
+
+    const  home =   document.getElementById('home')
+    home.innerText = category[7].category_name
     const  first =   document.getElementById('first')
     first.innerText = category[0].category_name
     const  second =   document.getElementById('second')
@@ -23,48 +36,73 @@ fetch('https://openapi.programming-hero.com/api/news/categories')
     const  eighth =   document.getElementById('th8')
     eighth.innerText = category[7].category_name
 }
+
+document.getElementById('home').addEventListener('click', function(){
+    // if (this.click) {
+    //     const color = document.getElementById('home')
+    //     color.style.backgroundColor = 'gray'
+    // }
+    // else{
+    //     color.style.backgroundColor = ''
+    // }
+    
+    fetch('https://openapi.programming-hero.com/api/news/category/08')
+    .then(res => res.json())
+    .then(data => cardDataLoad(data.data))
+    loader(true)
+    })
+
 document.getElementById('first').addEventListener('click', function(){
+    const color = document.getElementById('first')
+    color.style.backgroundColor = 'gray'
 fetch('https://openapi.programming-hero.com/api/news/category/01')
 .then(res => res.json())
 .then(data => cardDataLoad(data.data))
 loader(true)
 })
+
 document.getElementById('second').addEventListener('click', function(){
 fetch('https://openapi.programming-hero.com/api/news/category/02')
 .then(res => res.json())
 .then(data => cardDataLoad(data.data))
 loader(true)
 })
+
 document.getElementById('rd3').addEventListener('click', function(){
 fetch('https://openapi.programming-hero.com/api/news/category/03')
 .then(res => res.json())
 .then(data => cardDataLoad(data.data))
 loader(true)
 })
+
 document.getElementById('th4').addEventListener('click', function(){
 fetch('https://openapi.programming-hero.com/api/news/category/04')
 .then(res => res.json())
 .then(data => cardDataLoad(data.data))
 loader(true)
 })
+
 document.getElementById('th5').addEventListener('click', function(){
 fetch('https://openapi.programming-hero.com/api/news/category/05')
 .then(res => res.json())
 .then(data => cardDataLoad(data.data))
 loader(true)
 })
+
 document.getElementById('th6').addEventListener('click', function(){
 fetch('https://openapi.programming-hero.com/api/news/category/06')
 .then(res => res.json())
 .then(data => cardDataLoad(data.data))
 loader(true)
 })
+
 document.getElementById('th7').addEventListener('click', function(){
 fetch('https://openapi.programming-hero.com/api/news/category/07')
 .then(res => res.json())
 .then(data => cardDataLoad(data.data))
 loader(true)
 })
+
 document.getElementById('th8').addEventListener('click', function(){
 fetch('https://openapi.programming-hero.com/api/news/category/08')
 .then(res => res.json())
@@ -85,30 +123,48 @@ const loader = Loading => {
 }
 
 const cardDataLoad = cardData =>{
+    console.log(cardData)
+            const length = cardData.length
+            if (length > 0) {
+                const items = length +" " + 'News founded based on this category'
+                const found= document.getElementById('found')
+                found.innerText = items
+                
+            }
+            else{
+                const sorry = 'Sorry!!!! no news found '
+                const found= document.getElementById('found')
+                found.innerText = sorry
+                
+            }
             const viewCard = document.getElementById('cards')
             viewCard.innerHTML = ''
             loader(false)
+
+            cardData.sort((big , small) => {
+                return small.total_view - big.total_view;
+             });
             // console.log(cardData)
         cardData.forEach(newsOnCard =>{
                 const viewCardData = document.createElement('div') 
                 viewCardData.classList.add('col')
                 viewCardData.innerHTML = `
-                <div class="card h-100">
-                    <img src="${newsOnCard.image_url}" class="card-img-top" alt="...">
+                <div class="card h-100 shadow rounded">
+                    <img src="${newsOnCard.image_url ? newsOnCard.image_url : 'Did not find'}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${newsOnCard.title}</h5>
-                    <p class="card-text">${newsOnCard.details}</p>
+                    <h5 class="card-title title-height">${newsOnCard.title ? newsOnCard.title : 'Did not find'}</h5>
+                    <p class="card-text">${newsOnCard.details.slice(0,200) ? newsOnCard.details.slice(0, 200) : 'Did not find'}...</p>
                      <div class="d-flex justify-content-between">
                         <div class=' d-flex pt-2'>
-                            <img src="${newsOnCard.author.img}" class="" alt="..." style="height:45px; width:44px;border-radius: 50%;
+                            <img src="${newsOnCard.author.img ? newsOnCard.author.img : 'Did Not find'}" class="" alt="..." style="height:45px; width:44px;border-radius: 50%;
                             ">
                             <div>
-                            <p class='ms-2'>${newsOnCard.author.name} <br> ${newsOnCard.author.published_date}</p>
+                            <p class='ms-2'>${newsOnCard.author.name ? newsOnCard.author.name : 'Did not find'} <br> ${newsOnCard.author.published_date ? newsOnCard.author.published_date : 'Did not find'}</p>
                             
                             </div>
                         </div >
-                            <h6 class="pt-4">'${newsOnCard.total_view}'</h6>
-                            <button class='btn btn-info' style="height:50px;" onclick="loadCardDetailFull('${newsOnCard._id}')" data-bs-toggle="modal"  data-bs-target="#staticBackdrop"> see more </button>
+                            <h6 class="pt-4">'<i class="fa-solid fa-eye"></i> ${newsOnCard.total_view}'</h6>
+                            <button class='btn btn-info' style="height:50px;" onclick="loadCardDetailFull('${newsOnCard._id ? newsOnCard._id : 'Did not find'}')" data-bs-toggle="modal"  data-bs-target="#staticBackdrop"> see more </button>
                         </div>
                     </div> 
                 </div>   
@@ -117,7 +173,8 @@ const cardDataLoad = cardData =>{
         })
         
 }
-const loadCardDetailFull =async id =>{
+
+const loadCardDetailFull = async id =>{
     const urls = `https://openapi.programming-hero.com/api/news/${id}`
     fetch(urls)
     .then(res => res.json())
@@ -130,5 +187,7 @@ const loadCardDetailFull =async id =>{
         detailsModal.innerText = detail[0].details
             
     }
+
+
 
 
